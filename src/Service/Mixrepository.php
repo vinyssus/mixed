79c13bc1d4ce8c70400d6ae0903c91e3a638a3cb
vinyssus <vinyssusrodolphe@gmail.com>
2023-02-06 18:51:14 +0100

    use api in external file

--- src/Service/Mixrepository.php
+++ src/Service/Mixrepository.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -10,14 +11,14 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository{
 
-    private $httpClient;
+    private $githubContentClient;
     private $cache;
     // private bool $isDebug;
     //private $defauld_langue;
     
-    public function __construct(HttpClientInterface $httpClient,CacheInterface $cache, private bool $isDebug, private String $defauld_langue)
+    public function __construct(HttpClientInterface $githubContentClient,CacheInterface $cache,#[Autowire('%kernel.debug%')]private bool $isDebug, #[Autowire('%kernel.default_locale%')]private String $defauld_langue)
     {
-        $this->httpClient = $httpClient;
+        $this->githubContentClient = $githubContentClient;
         $this->cache = $cache;
         $this->isDebug = $isDebug;
         $this->defauld_langue = $defauld_langue;
@@ -27,14 +28,14 @@ class MixRepository{
     {
         return $this->cache->get('cache_data', function(CacheItemInterface $cacheItem){
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-            $response = $this->httpClient->request('GET','https://gist.githubusercontent.com/vinyssus/ab8f424657e1fc2827644589041129cc/raw/2336904b2d9a673b93a76bd718b0fb550415a771/mixed.json');
+            $response = $this->githubContentClient->request('GET','/vinyssus/ab8f424657e1fc2827644589041129cc/raw/2336904b2d9a673b93a76bd718b0fb550415a771/mixed.json');
             // $message = $this->translate->trans('Symfony is great');
             // echo $message;
-            if ($this->defauld_langue == 'en'){
-                echo 'anglais';
-            }else{
-                echo 'francais';
-            }
+            // if ($this->defauld_langue == 'en'){
+            //     echo 'anglais';
+            // }else{
+            //     echo 'francais';
+            // }
             return $response->toArray();
            });
     }
