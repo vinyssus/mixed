4de570a054f737ed8010fd140953b23e07d34a1b
vinyssus <vinyssusrodolphe@gmail.com>
2023-02-13 22:34:01 +0100

    forever

--- src/Controller/MixeController.php
+++ src/Controller/MixeController.php
@@ -16,7 +16,8 @@ class MixeController extends AbstractController
         $mix = new VinylMixe();
         $mix->setTitle('Do you Remember... Phil Collins?!');
         $mix->setDescription('A pure mix of drummers turned singers!');
-        $mix->setGenre('pop');
+        $genres = ['pop', 'rock'];
+        $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
         $entityManager->persist($mix);
